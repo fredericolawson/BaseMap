4eaca330c816aaf1069725ec5e3a4a83d07a70b2
Frederick Lawson <freddie@corneliajames.com>
2025-02-17 16:41:54 +0000

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,26 @@ import Link from 'next/link'
 export const metadata: Metadata = {
   title: 'BaseMap - Airtable Schema Visualizer',
   description: 'A simple Airtable schema visualization tool',
+  icons: {
+    icon: '/favicon.ico',
+  },
+  openGraph: {
+    title: 'BaseMap - Airtable Schema Visualizer',
+    description: 'A simple Airtable schema visualization tool',
+    type: 'website',
+    images: [{
+      url: '/pencil.png',
+      width: 512,  // adjust based on actual image dimensions
+      height: 512, // adjust based on actual image dimensions
+      alt: 'BaseMap Icon'
+    }],
+  },
+  twitter: {
+    card: 'summary',  // using 'summary' since we're using an icon
+    title: 'BaseMap - Airtable Schema Visualizer',
+    description: 'A simple Airtable schema visualization tool',
+    images: ['/pencil.png'],
+  }
 }
 
 export default function RootLayout({
